7e2128ad0dccff9d7ec560670cd12c5f143b51a8
WY1479052860 <1479052860@qq.com>
2020-02-21 19:46:23 +0800

    这是基类2

--- app/src/main/java/com/bawei/dimensionbusiness/activity/RegisterActivity.java
+++ app/src/main/java/com/bawei/dimensionbusiness/activity/RegisterActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.bawei.dimensionbusiness.Base.BaseActivity;
 import com.bawei.dimensionbusiness.R;
 import com.bawei.dimensionbusiness.bean.Register;
 import com.bawei.dimensionbusiness.utils.NetUtils;
@@ -17,7 +18,7 @@ import com.google.gson.Gson;
 
 import java.util.HashMap;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends BaseActivity {
 
     private EditText et_name;
     private EditText et_pwd;
@@ -25,14 +26,21 @@ public class RegisterActivity extends AppCompatActivity {
     String path="http://mobile.bwstudent.com/small/user/v1/register";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
-        Log.i("xxx",path);
+    protected int getLayoutResID() {
+        return R.layout.activity_register;
+    }
+
+    @Override
+    protected void initView() {
         //找控件
         et_name = findViewById(R.id.et_name);
         et_pwd = findViewById(R.id.et_pwd);
         bt_reg = findViewById(R.id.bt_reg);
+    }
+
+    @Override
+    protected void initData() {
+
         //点击事件
         bt_reg.setOnClickListener(new View.OnClickListener() {
             @Override
